d97b0a8109d45400afb5289a405d44474197f5ce
Sean Seefried <sean.seefried@gmail.com>
2019-05-05 21:27:09 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-rsync -avz index.html 'tilthydeath:/var/www/seanseefried.org'
+[ "$SEANSEEFRIED_DOT_ORG_MACHINE" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+[ "$SEANSEEFRIED_DOT_ORG_PATH" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+
+
+ssh "$SEANSEEFRIED_DOT_ORG_MACHINE" mkdir -p "$SEANSEEFRIED_DOT_ORG_PATH/resume"
+
+rsync -avz index.html "$SEANSEEFRIED_DOT_ORG_MACHINE:$SEANSEEFRIED_DOT_ORG_PATH/resume"
